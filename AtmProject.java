eb4a163e76a05f4f7c159fe0b84aee3ddc69b92f
Shakeeersk777 <116989444+Shakeeersk777@users.noreply.github.com>
2022-10-30 10:42:53 +0530

    Add files via upload

---
+++ AtmProject.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+class AtmProject
+{
+    public static void main(String[] args)
+    {
+        developProject();
+    }
+	public static void developProject()
+	{
+		Scanner scan = new Scanner(System.in);
+        int pin = 123456;
+
+        System.out.println("************ Welcome to Our Bank ************\n");
+
+        System.out.print("Enter PIN Number : ");   // User input for PIN Number
+        int customerPin = scan.nextInt();
+       
+        double balance = 50000.0;	// Default account balance
+
+        if (customerPin == pin)     // if user input matches with PIN
+        {
+            //options
+            System.out.println("1. Mini Statement");
+			System.out.println("2. Deposit");
+			System.out.println("3. Withdraw");
+			System.out.println("4. Exit");
+            System.out.println("Please select any one option :");
+            int option = scan.nextInt();
+
+            switch (option) 
+			{
+                case 1 :    //for ministatement
+                {
+                    System.out.println("Your current balance in your account : " + balance);
+                    tq();
+                }
+				break;
+
+                case 2 :     //for deposit
+                {
+                    System.out.print("Enter your deposit amount : ");
+                    double deposit = scan.nextInt();
+                    double totalAmount = deposit + balance;
+                    System.out.println("Your current balance in your account : " + totalAmount);
+                    tq();
+                }
+				break;
+
+                case 3 :    //for withdrawal
+                {
+                    System.out.print("Enter your withdrawal amount : ");
+                    double withdraw = scan.nextInt();
+					if (withdraw<100)
+					{
+						System.out.println("Please enter amount above 100 only");
+					}
+					else if (withdraw>=balance)
+					{
+						System.out.println("Insufficient funds");
+					}
+					else
+					{
+						double currentAmount = balance - withdraw;
+						System.out.println("Your current balance in your account : " + currentAmount);
+						tq();
+					}
+                }
+				break;
+
+                case 4 :
+                {
+                   break;
+                }
+                default: System.out.println("Please select valid option");
+            }
+        }
+        //if user input not matched then else will be executed
+        else
+        {
+            System.out.println("Invalid PIN");
+        }
+	}
+    // developer defined static method for wish
+    public static void tq()
+    {
+        System.out.println("***** Thank you for using our ATM *****");
+    }
+}
